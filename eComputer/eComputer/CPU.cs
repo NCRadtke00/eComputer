a321fe9a308b8c96871a36b95477b2e95678cf3a
NCRadtke00 <raddnc@gmail.com>
2020-08-12 16:00:23 -0500

    cpu class added

---
+++ eComputer/eComputer/CPU.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eComputer
+{
+    class CPU
+    {
+        public string manufacturer;
+        public string name;
+        public CPU(string newName, string manufacturer)
+        {
+            this.name = newName;
+            this.manufacturer = manufacturer;
+        }
+    }
+}
