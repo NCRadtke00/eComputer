e4088ff17099a3f2ae34b3f1682a9d07eeb7085d
NCRadtke00 <raddnc@gmail.com>
2020-08-12 17:03:13 -0500

    added ram class

---
+++ eComputer/eComputer/Ram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eComputer
+{
+    class Ram
+    {
+        double totalGigabytes;
+        string brand;
+        public Ram(double totalGigabytes, string brand)
+        {
+            this.totalGigabytes = totalGigabytes;
+            this.brand = brand;
+        }
+    }
+}
