bd996431921f5bed60a3c33582a331206d4bbb2d
NCRadtke00 <raddnc@gmail.com>
2020-08-12 17:01:00 -0500

    hard drive class created

---
+++ eComputer/eComputer/HardDrive.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eComputer
+{
+    class HardDrive
+    {
+        double totalStorage;
+        double availableStorage;
+
+        public HardDrive(double totalStorage, double availableStorage)
+        {
+            this.totalStorage = totalStorage;
+            this.availableStorage = availableStorage;
+        }
+    }
+}
